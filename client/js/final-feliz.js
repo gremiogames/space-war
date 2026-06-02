037a9b3f914b461dd63973e8cbda7ce91dec62a8
GUSTAVO LOCKS <gustavo.l09@aluno.ifsc.edu.br>
2026-06-02 14:09:26 +0000

    Tijolinho

--- client/js/final-feliz.js
+++ client/js/final-feliz.js
@@ -1,5 +1,42 @@
 /*global Phaser, axios*/
 /*eslint no-undef: "error"*/
+const BRICK_CREDIT_STREAK_KEY = "spaceWarBrickCreditStreak";
+
+function canUseStorage() {
+  return (
+    typeof window !== "undefined" && typeof window.localStorage !== "undefined"
+  );
+}
+
+function getBrickCreditStreak() {
+  if (!canUseStorage()) return 0;
+
+  const storedValue = window.localStorage.getItem(BRICK_CREDIT_STREAK_KEY);
+  const parsedValue = Number.parseInt(storedValue, 10);
+
+  return Number.isNaN(parsedValue) || parsedValue < 0 ? 0 : parsedValue;
+}
+
+function setBrickCreditStreak(value) {
+  if (!canUseStorage()) return value;
+
+  const normalizedValue = Number.isFinite(value) && value > 0 ? value : 0;
+  window.localStorage.setItem(
+    BRICK_CREDIT_STREAK_KEY,
+    String(normalizedValue),
+  );
+
+  return normalizedValue;
+}
+
+function getBrickCreditValue() {
+  const nextStreak = setBrickCreditStreak(getBrickCreditStreak() + 1);
+
+  if (nextStreak === 1) return 150;
+  if (nextStreak >= 8) return 25;
+  return 50;
+}
+
 export default class finalFeliz extends Phaser.Scene {
   constructor() {
     super("finalFeliz");
@@ -13,12 +50,14 @@ export default class finalFeliz extends Phaser.Scene {
         if (res.error) {
           console.error(res.error);
         } else {
+          const creditValue = getBrickCreditValue();
+
           axios
             .post(
               "https://feira-de-jogos.dev.br/api/v2/credit",
               {
                 product: 67, // id do jogo cadastrado no banco de dados da Feira de Jogos
-                value: this.game.placar=0, // crédito em tijolinhos
+                value: creditValue, // crédito em tijolinhos
               },
               {
                 headers: {
@@ -28,7 +67,7 @@ export default class finalFeliz extends Phaser.Scene {
             )
             .then(function (response) {
               console.log(response);
-              alert("Crédito adicionado!");
+              alert(`Crédito adicionado! +${creditValue}`);
             })
             .catch(function (error) {
               console.error(error);
